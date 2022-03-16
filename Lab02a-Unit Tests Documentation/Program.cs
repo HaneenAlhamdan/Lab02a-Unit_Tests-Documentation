b0ee4152b2871b162145c5ba56fdbbd4df033d8b
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-03-16 10:52:55 +0300

    Add project files.

---
+++ Lab02a-Unit Tests Documentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab02a_Unit_Tests_Documentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
