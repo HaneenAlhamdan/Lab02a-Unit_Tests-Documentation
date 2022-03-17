04a3db09d82de0fef5a9345331d306521aeecec9
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-03-17 10:54:51 +0300

    not done

--- Unit-Testing/UnitTest1.cs
+++ Unit-Testing/UnitTest1.cs
@@ -10,23 +10,9 @@ namespace Unit_Testing
         [Fact]
         public void Test1()
         {
-            [Theory]
-            [InlineData(200)]
-            [InlineData(400)]
-            public void canTEstWithdraw(decimal value)
-            {
-                decimal withdraw = Program.Withdraw(value);
-                Assert.Equal(withdraw, Program.ViewBalance());
-            }
-
-            [Theory]
-            [InlineData(200)]
-            [InlineData(400)]
-            public void canTestDeposit(decimal value)
-            {
-                decimal deposit = Program.Deposit(value);
-                Assert.Equal(deposit, Program.ViewBalance());
-            }
+           
+          
+            
         }
     }
 }
