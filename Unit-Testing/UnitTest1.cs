fb962e131447d01406499a9d19b450a06d247e2b
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-03-18 21:08:51 +0300

    done

--- Unit-Testing/UnitTest1.cs
+++ Unit-Testing/UnitTest1.cs
@@ -7,19 +7,13 @@ namespace Unit_Testing
 {
     public class UnitTest1
     {
-       
-            [Fact]
-            public void Test_view_Balance()
-            {
-              Assert.Equal(Program.balance, Program.ViewBalance());
-            }
 
 
-            [Fact]
-            public void Test_deposit_less_than_zero()
-           {
-              Assert.Equal(Program.balance , Program.Deposit(-99));
-            }
+        [Fact]
+        public void Test_deposit_less_than_zero()
+        {
+            Assert.Equal(Program.balance, Program.Deposit(-99));
+        }
 
 
         [Fact]
