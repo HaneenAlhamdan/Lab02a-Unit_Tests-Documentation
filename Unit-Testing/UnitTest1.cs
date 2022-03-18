0f25ba840bd72589ab8b7fcf8b4ef5de74fbbc48
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-03-18 20:53:11 +0300

    done

--- Unit-Testing/UnitTest1.cs
+++ Unit-Testing/UnitTest1.cs
@@ -1,18 +1,31 @@
 using System;
 using Xunit;
-using Unit_Testing;
+using Lab02a_Unit_Tests_Documentation;
 
 
 namespace Unit_Testing
 {
     public class UnitTest1
     {
+
         [Fact]
-        public void Test1()
+        public void Test_deposit_less_than_zero()
         {
-           
-          
-            
+            Assert.Equal(Program.balance, Program.Deposit(-99));
         }
-    }
+
+
+        [Fact]
+        public void Test_withdraw_grater_than_balance()
+
+        {
+            Assert.Equal(Program.balance, Program.Withdraw(245));
+        }
+
+
+        [Fact]
+        public void Test_withdraw_less_thanz_ero()
+        {
+            Assert.Equal(Program.balance, Program.Withdraw(-4));
+        }
 }
